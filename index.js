48a0ed3f6ed7647e50694b047b1e00a42a25d63e
saisob <saisob32@gmail.com>
2024-11-07 21:39:24 +0600

    create a new collectin

--- index.js
+++ index.js
@@ -18,16 +18,32 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // create a new collection 
+    const coffeeCollection = client.db('coffeeDB').collection('coffee');
+
+    app.post('/Coffee',async(req,res)=>{
+      const newCoffee = req.body;
+      console.log(newCoffee);
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
