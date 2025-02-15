fd6af68e832ba089edcf6c2632af85886bfa10d9
saisob <saisob32@gmail.com>
2025-02-15 08:46:36 +0600

    again connect mongoDB

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000;
+app.use(express.json());
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 
 
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yj3a8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yj3a8.mongodb.net/coffee?retryWrites=true&w=majority&appName=Cluster0`;
+// console.log(uri)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -20,23 +20,34 @@ const client = new MongoClient(uri, {
 
 
 
-
-
-
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
     // create a new collection 
-    const coffeeCollection = client.db('coffeeDB').collection('coffee');
+    const coffeeCollection = client.db('coffees').collection('coffee');
 
-    app.post('/Coffee',async(req,res)=>{
+    app.post('/coffee',async(req,res)=>{
       const newCoffee = req.body;
       console.log(newCoffee);
-      const result = await coffeeCollection.insertOne(newCoffee);
+      const result = await coffeeCollection.insertOne(newCoffee,{new:true});
+      // res.send(result);
+      res.status(201).json({
+        success: true,
+        message: "Coffee added successfully!",
+        data: { _id: result.insertedId, ...newCoffee }
+    });
+  
+
+    })
+
+    app.get("/a",async(req,res)=>{
+      const result = await coffeeCollection.find().toArray();
+      console.log(result)
       res.send(result);
     })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -59,4 +70,4 @@ app.get('/', (req,res) => {
 app.listen(port,()=>{
     console.log(`coffee server is running on port:${port}`)
 })
-
+  
